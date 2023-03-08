06384661b0ff92c050afc3432fceea787912fe93
dhannyamohangeetha <dhannyabrijesh.ktm@gmail.com>
2023-03-08 00:21:17 -0500

    Completed FileIO

--- Step4/ReadFour.java
+++ Step4/ReadFour.java
@@ -1,18 +1,24 @@
 package Step4;
 
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
 
-public class ReadFour
-{
+public class ReadFour {
     /**
-     * Yes, you can look at the other 
+     * Yes, you can look at the other
      * examples in this lab to build your solution here.
      */
-        public static void main(String[] args)
-        {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scan = new Scanner(new File("/Users/dhannya/Projects/FileIOJava/Step4/input.txt"));
+        int sum = 0;
+        while (scan.hasNext()) {
+            int lineIn = Integer.parseInt(scan.nextLine());
+            sum = sum + lineIn;
+            System.out.println("Running total: " + sum);
             // Print out a running total of all the
             // values in the input file.
         }
+    }
 }
