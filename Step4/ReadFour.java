a535976955b82f2c8cd59f213f799d2bdbb1feb6
deanOfWalls <deanofwalls@gmail.com>
2023-03-11 19:02:52 -0500

    checkpointing my work

--- Step4/ReadFour.java
+++ Step4/ReadFour.java
@@ -1,17 +1,27 @@
 package Step4;
 
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.io.File;
-import java.io.IOException;
 
 public class ReadFour
 {
     /**
-     * Yes, you can look at the other 
+     * Yes, you can look at the other
      * examples in this lab to build your solution here.
+     *
+     * @return
      */
-        public static void main(String[] args)
-        {
+        public static int main(String[] args) throws FileNotFoundException {
+            int totalFiles = 0;
+            Scanner fileIn = new Scanner(new File("input.txt"));
+            while (fileIn.hasNext());{
+                totalFiles++;
+
+        }
+        return totalFiles;
+
+
             // Print out a running total of all the
             // values in the input file.
         }
